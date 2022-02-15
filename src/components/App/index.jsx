1141dd3344f477f44a2aa18a519bd323f63844ac
Nick <nicholasppace@gmail.com>
2022-02-15 14:15:34 -0500

    add eslint

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Edit
+          <code> src/App.js </code>
+          and save to reload.
         </p>
         <a
           className="App-link"
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from './components/App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
